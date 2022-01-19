ae039ef6747e43d46411cc376768e1c8b92a697d
snehasis <snehasisdebbarman2016@gmail.com>
2022-01-19 20:50:43 +0530

    animate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./Nav";
+import "animate.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Nav
+        className="absolute top-0 left-0 right-0 "
+      />
+      <div className=" bg-teal-800  grid place-items-center ">
+        
+          <div className="text-rose-100 m-40 text-7xl animate__animated animate__hinge animate__jackInTheBox ">
+            Hello ,<br />
+            I'm{" "}
+            <strong class=" text-amber-300 ">
+              Snehasis Debbarman.
+            </strong>
+            <br />I am a Frontend Web Developer.
+          </div>
+          <div className="m-10">
+
+          </div>
+
+      </div>
     </div>
   );
 }
